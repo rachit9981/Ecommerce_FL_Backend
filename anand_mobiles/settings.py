3781bec691a2e03e563a633f30771c71fe8752f1
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-05-26 11:39:57 +0530

    Added secret key debugging bro

--- anand_mobiles/settings.py
+++ anand_mobiles/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'a_default_secret_key_for_development_123!@#') # Provide a default
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
